439621910e60ec62eb3d9a33c1e4824abf674e5c
varsha-kadaru <varshakadaru@gmail.com>
2024-06-09 14:14:28 +0530

    add

---
+++ Hello.java
@@ -0,0 +1,6 @@
+package Jenkins;
+public class Hello{
+    public static void main(String args[]){
+        System.out.println("Hello 1");
+    }
+}
---
+++ Hello2.java
@@ -0,0 +1,6 @@
+package Jenkins;
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world 2");
+    }
+}
